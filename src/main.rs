27d8f99d78fbcc3f8f173661f555b42b40d19054
zohassadar <zohassadar@gmail.com>
2024-07-07 11:59:37 -0700

    ugh

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use gumdrop::Options;
 use indexmap::IndexMap;
-use std::fs::File;
-use std::process::Command;
-use std::io::prelude::*;
+// use std::fs::File;
+// use std::io::prelude::*;
+// use std::process::Command;
 mod rng;
-mod shuffled;
+
+/*
 pub fn write_pre_shuffle() -> std::io::Result<()> {
     let mut file = File::create("src/shuffled.rs")?;
     // one roll index
@@ -17,45 +18,67 @@ pub fn write_pre_shuffle() -> std::io::Result<()> {
     // create shortcut arrays using one roll index
     let mut by_repeats: [[u16; 0x10000]; 0x10] = [[0; 0x10000]; 0x10];
     for index in 0..16 {
-        let repeats = index + 3;
+        let mut repeats = 16;
+        if index != 0 {
+            repeats = index;
+        }
+        repeats += 3;
         for i in 0..=0xFFFF {
             let mut s = i;
-            for _ in 0..repeats {
+            for _ in 0..=repeats {
                 s = rng::shuffle_rng(i as u16)
             }
             by_repeats[index][i as usize] = s;
             // dbg!(by_repeats[index][i as usize]);
         }
     }
+
+    file.write_all(b"pub static SHUFFLED_ONCE: [u16; 0x10000] = [\n")?;
+    for seed in shuffled {
+
+            file.write_all(format!("    0x{seed:04X},\n").as_bytes())?;
+    }
+    file.write_all(b"];")?;
+
     file.write_all(b"pub static BY_REPEATS: [[u16; 0x10000]; 0x10] = [\n")?;
     for index in 0..16 {
         file.write_all(b"    [\n")?;
         for seed_idx in 0..=0xFFFF {
             let seed = by_repeats[index][seed_idx];
-            file.write_all(format!("        0x{seed:04X},\n").as_bytes())?;
         }
         file.write_all(b"    ],\n")?;
     }
     file.write_all(b"];")?;
 
-    let _ = Command::new("rustfmt").arg("src/shuffled.rs").output().expect("");
+    let _ = Command::new("rustfmt")
+        .arg("src/shuffled.rs")
+        .output()
+        .expect("");
     Ok(())
 }
-
+*/
 fn crunch_seed(
     seed1: u8,
     seed2: u8,
     seed3: u8,
-    known_seeds: &mut IndexMap<(u8, u8, u8), (u32, u32, (u8, u8, u8, u8))>,
-    known_loops: &mut IndexMap<(u8, u8, u8, u8), IndexMap<(u8, u8, u8, u8), (u8, u8, u8, u8)>>,
-    orientation_ids: &[u8; 0x7],
+    known_seeds: &mut IndexMap<(u8, u8, u8), (u32, u32, (u8, u8, u8, u8, u8))>,
+    known_loops: &mut IndexMap<
+        (u8, u8, u8, u8, u8),
+        IndexMap<(u8, u8, u8, u8, u8), (u8, u8, u8, u8, u8)>,
+    >,
+    shuffled: &Vec<u16>,
+    by_repeats: &Vec<Vec<u16>>,
 ) {
     let og = (seed1, seed2 & 0xFE, seed3 & 0xF7);
+    if og.0 == 0 && og.1 == 0 {
+        return;
+    }
+    // dbg!(og);
     if known_seeds.contains_key(&og) {
         return;
     }
     let mut steps = 0;
-    let mut path: IndexMap<(u8, u8, u8, u8), (u8, u8, u8, u8)> = IndexMap::new();
+    let mut path: IndexMap<(u8, u8, u8, u8, u8), (u8, u8, u8, u8, u8)> = IndexMap::new();
     let mut repeats = 18;
     if (seed3 >> 4) > 0 {
         repeats = (seed3 >> 4) + 2;
@@ -65,26 +88,25 @@ fn crunch_seed(
     let mut s2 = seed2;
     let mut s3 = seed3;
     loop {
-        let original = (s1, s2, s3, spawn_id);
-        // dbg!(original);
+        let id = (s1, s2 & 0xFE, s3 & 0x07, spawn_id, repeats);
+        //dbg!(original);
         for (loop_id, loop_map) in known_loops.into_iter() {
-            if loop_map.contains_key(&original.clone()) {
-                let index = loop_map
-                    .into_iter()
-                    .position(|(&l, _)| l == original)
-                    .unwrap();
+            if loop_map.contains_key(&id) {
+                let index = loop_map.into_iter().position(|(&l, _)| l == id).unwrap();
                 // println!("New seed found: {:#?}", loop_id);
                 known_seeds.insert(og, (steps as u32, index as u32, *loop_id));
                 return;
             }
         }
         // dbg!(s1, s2, s3, spawn_id);
-        let new = rng::get_next_piece(s1, s2, s3, spawn_id, repeats, &orientation_ids);
-        (s1, s2, s3, spawn_id) = new;
+        (s1, s2, s3, spawn_id, _) =
+            rng::get_next_piece(s1, s2, s3, spawn_id, repeats, &shuffled, &by_repeats);
         // dbg!(s1, s2, s3, spawn_id);
-        let new = (s1, s2, s3, spawn_id);
+        let new = (s1, s2 & 0xFE, s3 & 0x7, spawn_id, repeats);
+        // dbg!(path.clone());
         if path.contains_key(&new) {
-            // dbg!(path.clone());
+            dbg!(og);
+            dbg!(path.clone().keys().len());
             let loop_start = path
                 .clone()
                 .into_iter()
@@ -92,8 +114,10 @@ fn crunch_seed(
                 .unwrap();
             let mut new_loop = IndexMap::new();
             // dbg!(loop_start);
-            for (k, v) in path.clone().into_iter() {
-                let (i, _, _) = path.clone().get_full(&k).unwrap();
+            for (i, (k, v)) in path.clone().into_iter().enumerate() {
+                // slow!!
+                // let (i, _, _) = path.clone().get_full(&k).unwrap();
+
                 // dbg!((i,(k,v)));
                 if i < loop_start {
                     continue;
@@ -101,17 +125,17 @@ fn crunch_seed(
                 // dbg!(i);
                 new_loop.insert(k, v);
             }
-            // dbg!(new_loop.clone());
-            known_loops.insert(new, new_loop);
+            // dbg!(new_loop.clone().keys().len());
+            known_loops.insert(new, new_loop.clone());
             known_seeds.insert(og, (steps as u32, 0, new));
             return;
         }
         // dbg!(path.clone());
-        steps += 32;
-        path.insert(original, new);
+        steps += 1;
+        path.insert(id, new);
     }
 }
-
+#[allow(dead_code)]
 fn parse_hex(s: &str) -> Result<u32, std::num::ParseIntError> {
     u32::from_str_radix(s, 16)
 }
@@ -125,23 +149,25 @@ struct TestOptions {
 }
 
 fn main() {
-    let options = TestOptions::parse_args_default_or_exit();
-    if options.make_table {
-        println!("Making new shuffle table");
-        let _ = write_pre_shuffle();
+    // let options = TestOptions::parse_args_default_or_exit();
+    // if options.make_table {
+    //     println!("Making new shuffle table");
+    //     let _ = write_pre_shuffle();
 
-        return;
-    }
-    let orientation_ids: [u8; 0x7] = [0x02, 0x07, 0x08, 0x0A, 0x0B, 0x0E, 0x12];
-    let mut known_seeds: IndexMap<(u8, u8, u8), (u32, u32, (u8, u8, u8, u8))> = IndexMap::new();
-    let mut known_loops: IndexMap<(u8, u8, u8, u8), IndexMap<(u8, u8, u8, u8), (u8, u8, u8, u8)>> =
-        IndexMap::new();
-
+    //     return;
+    // }
+    let (shuffled, by_repeats) = rng::get_pre_shuffle();
+    let mut known_seeds: IndexMap<(u8, u8, u8), (u32, u32, (u8, u8, u8, u8, u8))> = IndexMap::new();
+    let mut known_loops: IndexMap<
+        (u8, u8, u8, u8, u8),
+        IndexMap<(u8, u8, u8, u8, u8), (u8, u8, u8, u8, u8)>,
+    > = IndexMap::new();
 
     println!("Hello, world!");
-    for x in 0..=0xF {
+    for x in 0..=0x1 {
         println!("x is {}", x);
-        for y in 0..=0xFF {
+        for y in 0..=0xF {
+            println!("y is {}", y);
             for z in 0..=0xFF {
                 crunch_seed(
                     x,
@@ -149,7 +175,8 @@ fn main() {
                     z,
                     &mut known_seeds,
                     &mut known_loops,
-                    &orientation_ids,
+                    &shuffled,
+                    &by_repeats,
                 );
             }
         }
@@ -163,11 +190,12 @@ fn main() {
 
     for (loop_id, loop_map) in known_loops.into_iter() {
         println!(
-            "Loop id {} {} {} {} - {}",
+            "Loop id {} {} {} {} {} - {}",
             loop_id.0,
             loop_id.1,
             loop_id.2,
             loop_id.3,
+            loop_id.4,
             loop_map.keys().len(),
         );
     }
--- src/rng.rs
+++ src/rng.rs
@@ -1,9 +1,41 @@
-use crate::{shuffled};
-
+const ORIENTATION_IDS: [u8; 0x7] = [0x02, 0x07, 0x08, 0x0A, 0x0B, 0x0E, 0x12];
 pub fn add_two(num: u16) -> u16 {
     return num + num;
 }
+pub fn get_pre_shuffle() -> (Vec<u16>, Vec<Vec<u16>>) {
+    let mut shuffled: Vec<u16> = vec![0; 0x10000];
+    for i in 0..0x10000 {
+        shuffled[i] = shuffle_rng(i as u16);
+        // dbg!(shuffled[i]);
+    }
 
+    // create shortcut arrays using one roll index
+    let mut by_repeats: Vec<Vec<u16>> = vec![vec![0; 0x10000]; 0x10];
+    for index in 0..0x10 {
+        let mut repeats = 0x10;
+        if index != 0 {
+            repeats = index;
+        }
+        repeats += 3;
+        for i in 0..=0xFFFF {
+            let mut s = i;
+            for _ in 0..repeats {
+                if index == 1 && i == 0x1111 {
+                    dbg!(repeats);
+                    println!("{:04X}", s);
+                }
+                s = shuffle_rng(s as u16)
+            }
+            if index == 1 && i == 0x1111 {
+                dbg!(repeats);
+                println!("{:04X}", s);
+            }
+            by_repeats[index][i as usize] = s;
+            // dbg!(by_repeats[index][i as usize]);
+        }
+    }
+    return (shuffled, by_repeats);
+}
 pub fn shuffle_rng(rng: u16) -> u16 {
     add_two(2);
     let rng_hi = rng >> 8;
@@ -15,31 +47,44 @@ pub fn shuffle_rng(rng: u16) -> u16 {
 }
 
 pub fn get_next_piece(
+    repeat_nybble: u8,
     seed1: u8,
     seed2: u8,
     seed3: u8,
     spawn_id: u8,
-    repeats: u8, // 3 to 18
-    orientation_ids: &[u8; 0x7],
-) -> (u8, u8, u8, u8) {
+    shuffled: &Vec<u16>,
+    by_repeats: &Vec<Vec<u16>>,
+) -> (u8, u8, u8, u8, u8) {
     // increment and rollover 3rd byte
-    let s3 = ((seed3 as u16 + 1) & 0xFF) as u8;
-
+    // dbg!(seed1);
+    // dbg!(seed2);
+    // dbg!(seed3);
+    // dbg!(spawn_id);
     // dbg!(repeats);
-    let roll = shuffled::BY_REPEATS[(repeats - 3) as usize][((seed1 as u16) << 8 | seed2 as u16) as usize];
-    let mut s1 = (roll << 8) as u8;
+    let s3 = ((seed3 as u16 + 1) & 0xFF) as u8;
+    dbg!(s3);
+    let rol_idx = ((seed1 as u16) << 8 | seed2 as u16) as usize;
+    // dbg!(rpt_idx);
+    // dbg!(rol_idx);
+    let roll = by_repeats[repeat_nybble as usize][rol_idx];
+    println!("roll={:04X}", roll);
+    // dbg!(shuffled::BY_REPEATS[rpt_idx][rol_idx]);
+    // dbg!(roll);
+    let mut s1 = (roll >> 8) as u8;
+    // dbg!(s1);
     let mut s2 = (roll & 0xFF) as u8;
-    let mut result = (s1 + s2) & 0x7;
-    if result == 7 || orientation_ids[result as usize] == spawn_id {
-        let reroll = shuffled::BY_REPEATS[(repeats - 3) as usize][((s1 as u16) << 8 | s2 as u16) as usize];
-        s1 = (reroll << 8) as u8;
+    // dbg!(s2);
+    let mut result = ((s1 as u16 + s2 as u16) & 0x7) as u8;
+    if result == 7 || ORIENTATION_IDS[result as usize] == spawn_id {
+        let reroll = shuffled[roll as usize];
+        s1 = (reroll >> 8) as u8;
         s2 = (reroll & 0xFF) as u8;
         result = (((s1 & 7) + spawn_id) & 0xFF) % 7;
     }
-    let new_id = orientation_ids[result as usize];
+    let new_id = ORIENTATION_IDS[result as usize];
 
     // clear out bits that don't do anything
-    let new = (s1, s2 & 0xFE, s3 & 0x7, new_id);
+    let new = (repeat_nybble, s1, s2 & 0xFE, s3 & 0x7, new_id);
     // dbg!(new);
     return new;
 }
@@ -51,5 +96,36 @@ mod tests {
     #[test]
     fn internal() {
         assert_eq!(4, add_two(2));
+    }
+    #[test]
+    fn test_lfsr() {
+        assert_eq!(0x84EF, shuffle_rng(0x09DF));
+        assert_eq!(0x5D43, shuffle_rng(0xBA87));
+        assert_eq!(0x0420, shuffle_rng(0x0840));
+        assert_eq!(0x7AEA, shuffle_rng(0xF5D4));
+        assert_eq!(0x0C10, shuffle_rng(0x1820));
+        assert_eq!(0x0000, shuffle_rng(0x0000));
+    }
+    #[test]
+    fn test_poop() {
+        let (shuffled, by_repeats) = get_pre_shuffle();
+        assert_eq!(0x84EF, shuffled[0x09DF]);
+        assert_eq!(0x84EF, shuffled[0x09DF]);
+        assert_eq!(0x5D43, shuffled[0xBA87]);
+        assert_eq!(0x0420, shuffled[0x0840]);
+        assert_eq!(0x7AEA, shuffled[0xF5D4]);
+        assert_eq!(0x0C10, shuffled[0x1820]);
+        assert_eq!(0x0000, shuffled[0x0000]);
+
+        assert_eq!(0x0111, by_repeats[0x1][0x1111]);
+    }
+    #[test]
+    fn test_next_piece_seed() {
+        let (shuffled, by_repeats) = get_pre_shuffle();
+        // repeat nybble, set_seed+0, set_seed+1, set_seed+2, spawn_id
+        assert_eq!(
+            (0x01, 0x01, 0x11, 0x02, 0x0A),
+            get_next_piece(0x01, 0x11, 0x11, 0x11, 0x00, &shuffled, &by_repeats)
+        );
     }
 }
